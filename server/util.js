4a604c122d60ac257cd0e09489e70e71d9c6cb7d
dannycho7 <dannycho7@gmail.com>
2018-08-04 17:56:22 -0700

    Invoke python stub scripts

--- server/util.js
+++ server/util.js
@@ -1,15 +1,20 @@
 const path = require("path");
 const fs = require("fs");
+const { exec } = require("child_process");
 
 const alignEncode = (imgBlob) => {
 	return new Promise((resolve, reject) => {
-		resolve(fs.readFileSync(path.join(__dirname, "sample_images/cropped.dump")));
+		exec(`python ${path.join(__dirname, "scripts/align_encode.py")} ${imgBlob}`, (err, stdout, stderr) => {
+			resolve(fs.readFileSync(path.join(__dirname, "sample_images/cropped.dump")));
+		});
 	});
 };
 
 const manipulateAll = (imgBlob, age, last_seen) => {
 	return new Promise((resolve, reject) => {
-		resolve(fs.readFileSync(path.join(__dirname, "sample_images/data_man.dump")));
+		exec(`python ${path.join(__dirname, "scripts/manipulate_all.py")} ${imgBlob} [] []`, (err, stdout, stderr) => {
+			resolve(fs.readFileSync(path.join(__dirname, "sample_images/data_man.dump")));
+		});
 	});
 };
 
