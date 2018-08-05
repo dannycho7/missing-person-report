a3b6edb8ffcc88b393f244c645f64fd85f914c66
dannycho7 <dannycho7@gmail.com>
2018-08-04 17:59:53 -0700

    rename transform args

--- server/util.js
+++ server/util.js
@@ -2,17 +2,17 @@ const path = require("path");
 const fs = require("fs");
 const { exec } = require("child_process");
 
-const alignEncode = (imgBlob) => {
+const alignEncode = (img_blob) => {
 	return new Promise((resolve, reject) => {
-		exec(`python ${path.join(__dirname, "scripts/align_encode.py")} ${imgBlob}`, (err, stdout, stderr) => {
+		exec(`python ${path.join(__dirname, "scripts/align_encode.py")} ${img_blob}`, (err, stdout, stderr) => {
 			resolve(fs.readFileSync(path.join(__dirname, "sample_images/cropped.dump")));
 		});
 	});
 };
 
-const manipulateAll = (imgBlob, age, last_seen) => {
+const manipulateAll = (z, age, last_seen) => {
 	return new Promise((resolve, reject) => {
-		exec(`python ${path.join(__dirname, "scripts/manipulate_all.py")} ${imgBlob} [] []`, (err, stdout, stderr) => {
+		exec(`python ${path.join(__dirname, "scripts/manipulate_all.py")} ${z} [] []`, (err, stdout, stderr) => {
 			resolve(fs.readFileSync(path.join(__dirname, "sample_images/data_man.dump")));
 		});
 	});
